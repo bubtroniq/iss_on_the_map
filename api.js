39bab91eec0968ea548a14dbd7e4f2defc4a46f5
Razvan Joitescu <joitescurazvan@gmail.com>
2023-01-12 12:50:40 +0000

    Updated api.js

--- api.js
+++ api.js
@@ -6,31 +6,31 @@
 
 // Making a map and tiles
 
-const mymap = L.map('harta').setView([0, 0], 1);
-        const marker = L.marker([0, 0]).addTo(mymap);
+    const mymap = L.map('harta').setView([0, 0], 1);
+    const marker = L.marker([0, 0]).addTo(mymap);
 
-const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+    const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
-const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileUrl, {attribution});
-tiles.addTo(mymap);
+        const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tiles = L.tileLayer(tileUrl, {attribution});
+    tiles.addTo(mymap);
 
 // Making a marker with an icon
 
-const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
-async function getISS() {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    const { latitude, longitude } = data;
+    const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+    async function getISS() {
+        const response = await fetch(api_url);
+        const data = await response.json();
+        const { latitude, longitude } = data;
 
 
-    // L.marker([latitude, longitude]).addTo(mymap);
-    
-    marker.setLatLng([latitude, longitude]);
-    mymap.setView([latitude, longitude], 1);
+        // L.marker([latitude, longitude]).addTo(mymap);
+        mymap.setView([latitude, longitude], 2);
+        marker.setLatLng([latitude, longitude]);
+        
 
-    document.getElementById('lat').textContent = latitude;
-    document.getElementById('lon').textContent = longitude;
+        document.getElementById('lat').textContent = latitude.toFixed(2);
+        document.getElementById('lon').textContent = longitude.toFixed(2);
 }
 
 getISS();
